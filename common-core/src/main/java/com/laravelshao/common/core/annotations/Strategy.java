da03389d19ad2be5fb79841bda6f6d0a476b4d3c
shaoqinghua <shaoqinghua@youyuwo.com>
2019-05-11 13:52:10 +0800

    init

---
+++ common-core/src/main/java/com/laravelshao/common/core/annotations/Strategy.java
@@ -0,0 +1,35 @@
+package com.laravelshao.common.core.annotations;
+
+import com.laravelshao.common.core.handler.StrategyHandler;
+
+import java.lang.annotation.*;
+
+/**
+ * 策略注解
+ *
+ * @author shaoqinghua
+ * @date 2019/5/11
+ * @description
+ * @see StrategyHandler
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface Strategy {
+
+    /**
+     * 策略代码
+     *
+     * @return
+     */
+    String strategyCode() default "";
+
+    /**
+     * 描述信息
+     *
+     * @return
+     */
+    String description() default "";
+
+}
---
+++ common-core/src/main/java/com/laravelshao/common/core/handler/StrategyHandler.java
@@ -0,0 +1,28 @@
+package com.laravelshao.common.core.handler;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * @author shaoqinghua
+ * @date 2019/5/11
+ * @description
+ */
+public class StrategyHandler implements InitializingBean, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+
+}
